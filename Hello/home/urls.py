d156c820e44a88b8ab6affab03db170f53c1e8c8
adhiraj <adhirajsingh735@gmail.com>
2024-05-12 22:59:43 +0530

    about function added in views

--- Hello/home/urls.py
+++ Hello/home/urls.py
@@ -4,4 +4,5 @@ from home import views         #import views from home
 
 urlpatterns = [
     path("", views.index, name="home"),            #if the path is empty, it will redirect to views.index function
+    path("about", views.about, name="about"),      #if the path is "about", it will redirect to views.about function
 ]
--- Hello/home/views.py
+++ Hello/home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,HttpResponse   #import HttpResponse from dja
 # Create your views here.
 def index(request):
     return HttpResponse("This is the homepage")    #return HttpResponse with the message "This is the homepage"
+
+def about(request):
+    return HttpResponse("This is the about page")   #return HttpResponse with the message "This is the about page"
